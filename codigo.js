6fc0c624540bee4bf1f92ffdbb66d2e71f10c6ac
Tomicastel <tomielcoria@hotmail.com>
2024-01-30 19:11:11 -0300

    muestro arreglo

--- codigo.js
+++ codigo.js
@@ -1,4 +1,15 @@
 console.log("Hola mundo!");
 
-const animales = ['perro','gato']
+const animales = ['perro', 'gato']
 
+var divAnimales = document.getElementById("animales")
+
+function mostrarAnimales() {
+    for (var i = 0; i < 2; i++) {
+        divAnimales.innerHTML += `
+    <p>${animales[i]}</p>
+    `;
+    }
+}
+
+window.onload = mostrarAnimales;
